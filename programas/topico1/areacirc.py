6d268be2b8580b0fbf3fdb11f8ddd8b8181100cc
paimvianna <paimvianna@gmail.com>
2024-12-20 13:38:47 -0300

    Update areacirc.py

--- programas/topico1/areacirc.py
+++ programas/topico1/areacirc.py
@@ -1,7 +1,7 @@
-#Entrada
+5#Entrada
 #from math import pi, pra usar esta biblioteca neste caso tenho que ver como definir a quantidade de casa decimais após o ponto.
 pi = 3.1415
-d = float(input("Por favor, informe o diametro do circulo em centimetros preferencialmente em valores inteiros: "))
+d = float(input("Por favor, informe o diametro do circulo em \ncentimetros preferencialmente em valores inteiros: "))
 
 #Processamento
 area = pi*((d/2)**2)
